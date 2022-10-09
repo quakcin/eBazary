16008018019e1abb613f0de7fb4bbf4ebfe6e3bb
Michael <michas2820@onet.eu>
2022-10-09 22:38:13 +0200

    Fixed navbar colors on device

--- utils/NavBar.js
+++ utils/NavBar.js
@@ -12,44 +12,51 @@ import { useTailwind } from 'tailwind-rn'
 const NavBar = ({ navigation, active }) => {
   const tw = useTailwind()
 
-  const colors =
-  {
-    Cart: '', NewOffer: '', Home: '', Bell: '', Profile: ''
+  const colors = {
+    Cart: '',
+    NewOffer: '',
+    Home: '',
+    Bell: '',
+    Profile: ''
   }
 
-  Object.keys(colors).map((v) => colors[v] = '#393939');
-  colors[active] = '#d62f64';
+  Object.keys(colors).map((v) => (colors[v] = '#393939'))
+  colors[active] = '#d62f64'
 
   return (
     <View style={tw('justify-around flex flex-row w-full bg-white p-3')}>
-
-      <ShoppingCartIcon 
-        style={tw(`text-[${colors.Cart}]`)} 
-        width={30} height={30} 
+      <ShoppingCartIcon
+        style={{ color: colors.Cart }}
+        width={30}
+        height={30}
         onPress={() => navigation.navigate('CartView')}
       />
 
-      <PlusIcon 
-        style={tw(`text-[${colors.NewOffer}]`)} 
-        width={30} height={30} 
+      <PlusIcon
+        style={{ color: colors.NewOffer }}
+        width={30}
+        height={30}
         onPress={() => navigation.navigate('NewOfferView')}
       />
 
       <ShoppingBagIcon
-        style={tw(`text-[${colors.Home}]`)}
-        width={30} height={30}
+        style={{ color: colors.Home }}
+        width={30}
+        height={30}
         onPress={() => navigation.navigate('HomeView')}
       />
 
-      <BellIcon 
-        style={tw(`text-[${colors.Bell}]`)} 
-        width={30} height={30} 
+      <BellIcon
+        style={{ color: colors.Bell }}
+        width={30}
+        height={30}
         onPress={() => navigation.navigate('BellView')}
       />
 
       <UserCircleIcon
-        style={tw(`text-[${colors.Profile}]`)}
-        width={30} height={30}
+        style={{ color: colors.Profile }}
+        width={30}
+        height={30}
         onPress={() => navigation.navigate('ProfileView')}
       />
     </View>
--- views/AuthView.js
+++ views/AuthView.js
@@ -3,10 +3,13 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useTailwind } from 'tailwind-rn/dist'
 
 export function AuthView({ navigation }) {
+  const tw = useTailwind()
+
   return (
-    <View>
+    <View style={tw('mt-10')}>
       <Button title='Zaloguj' onPress={() => navigation.navigate('HomeView')} />
     </View>
   )
