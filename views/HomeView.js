479bb8c1f49e07260ef930bf2453d0e63f14398b
MartyLocke <martyunderscore@gmail.com>
2022-10-09 21:17:11 +0200

    progress

--- views/HomeView.js
+++ views/HomeView.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useTailwind } from 'tailwind-rn'
 
+import { ShoppingBagIcon } from "react-native-heroicons/outline";
+
 import { NavBar } from './NavBar.js'
 
 export function HomeView({ navigation }) {
@@ -12,6 +14,10 @@ export function HomeView({ navigation }) {
 
   return (
     <View style={tw('bg-red-500')}>
+      <Text>
+        Hello World
+      </Text>
+      <ShoppingBagIcon width={100} height={100} style={tw('text-black')}/>
       <NavBar navigation={navigation} />
     </View>
   )
