4a99671aeca1c59a418b42f443af3a5fc45d1f79
bandurama <banduram1@gmail.com>
2022-10-12 23:47:13 +0200

    new file:   components/ImageSlider.js

---
+++ components/ImageSlider.js
@@ -0,0 +1,32 @@
+import { SafeAreaView, Text, View, Image, ScrollView, Touchable, TouchableOpacity } from 'react-native';
+import AutoHeightImage from 'react-native-auto-height-image';
+ 
+ 
+const VerticalSlider = ({pictures, navigation}) => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}>
+        {pictures.map((item) => (
+          <TouchableOpacity 
+            activeOpacity={1}
+            style={{justifyContent: "center"}}
+            onPress={() =>
+              navigation.navigate('showImage', {
+                url: item
+              })
+              }
+            >
+            <AutoHeightImage
+                width={300}
+                source={{uri: item}}
+                style={{margin: 20}}
+              />
+          </TouchableOpacity>
+            ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default VerticalSlider;
