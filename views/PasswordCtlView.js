22ce8f11db332a23826b438e4b5ff6d64b503fcb
bandurama <banduram1@gmail.com>
2022-10-16 19:45:03 +0200

    Change Password View

--- views/PasswordCtlView.js
+++ views/PasswordCtlView.js
@@ -1,6 +1,6 @@
 
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native'
 import { useTailwind } from 'tailwind-rn'
 import { Viewport } from '../utils/Viewport'
 
@@ -9,36 +9,37 @@ export function PasswordCtlView ({ navigation }) {
 
   return (
     <Viewport navigation={navigation} active="ProfileView">
-      <View style={styles.container}>
-        <TextInput
-          name="oldPas"
-          style={styles.input}
-          placeholder="Aktualne hasło"
-          secureTextEntry={true}
-        />
-        <TextInput
-          name="newPas1"
-          style={styles.input}
-          placeholder="Nowe hasło"
-          secureTextEntry={true}
-        />
-        <TextInput
-          name="newPas2"
-          style={styles.input}
-          placeholder="Powtórz nowe hasło"
-          secureTextEntry={true}
-        />
-      </View>
-      <View style={{padding: 35, marginTop: 10 }}>
-        <Button
-          title="Zmień hasło"
-          onPress={() => console.log('Zmiana hasła')}
-        />
-      </View>
-      <View style={styles.info}>
-        <Text style={styles.info}>Utrata hasła spowoduje utratę dostępu do konta!</Text>
-      </View>
-
+      <ScrollView>
+        <View style={styles.container}>
+          <TextInput
+            name="oldPas"
+            style={styles.input}
+            placeholder="Aktualne hasło"
+            secureTextEntry={true}
+          />
+          <TextInput
+            name="newPas1"
+            style={styles.input}
+            placeholder="Nowe hasło"
+            secureTextEntry={true}
+          />
+          <TextInput
+            name="newPas2"
+            style={styles.input}
+            placeholder="Powtórz nowe hasło"
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={{padding: 35, marginTop: 10 }}>
+          <Button
+            title="Zmień hasło"
+            onPress={() => console.log('Zmiana hasła')}
+          />
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.info}>Utrata hasła spowoduje utratę dostępu do konta!</Text>
+        </View>
+      </ScrollView>
     </Viewport>
   )
 }
