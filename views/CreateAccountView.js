518dc576b50d27581e76866f888f8b1e6c0f6ed9
bandurama <banduram1@gmail.com>
2022-10-16 23:33:17 +0200

    Prepare login and register view

--- views/CreateAccountView.js
+++ views/CreateAccountView.js
@@ -7,59 +7,57 @@ import { Viewport } from '../utils/Viewport'
 
 export function CreateAccountView ({ navigation }) {
   return (
-    <Viewport navigation={navigation} active="Cart">
-      <ScrollView style={{marginTop: "12%", padding: 20}}>
-        <View style={{alignItems:"center", marginBottom: "10%"}}>
-          <AutoHeightImage
-            source={{uri:"https://i.postimg.cc/9fW1Sg1Y/clipart323390.png"}}
-            width={140}
-          />
-          <View style = {{width: "100%", alignItems:"center"}}>
+    <ScrollView style={{marginTop: "12%", padding: 20}}>
+      <View style={{alignItems:"center", marginBottom: "10%"}}>
+        <AutoHeightImage
+          source={{uri:"https://i.postimg.cc/9fW1Sg1Y/clipart323390.png"}}
+          width={140}
+        />
+        <View style = {{width: "100%", alignItems:"center"}}>
 
-            <TextInput
-              name="name"
-              style={styles.input}
-              placeholder="Imię"
-            />
-            <TextInput
-              name="surname"
-              style={styles.input}
-              placeholder="Nazwisko"
-            />
-            <TextInput
-              name="nickname"
-              style={styles.input}
-              placeholder="Nazwa użytkownika"
-            />
-            <TextInput
-              name="mail"
-              style={styles.input}
-              placeholder="Adres e-mail"
-            />
-            <TextInput
-              name="pass1"
-              style={styles.input}
-              placeholder="Hasło"
-            />
-            <TextInput
-              name="pass2"
-              style={styles.input}
-              placeholder="Powtórz hasło"
-            />
-          </View>
-          <View style = {styles.buttonArea}>
-            <TouchableOpacity style={styles.button} onPress={() => console.log("Zapisz")} >
-              <Text style={{fontSize: 18}}>Zarejestruj się</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.info}>
-            <Text style={styles.info}>Jeżeli posiadasz już konto kliknij
-                <Text style={[styles.info, {color: '#b8215b'}]} onPress={() => { navigation.navigate('AuthView') }}> tutaj. </Text>
-            </Text>
-          </View>
+          <TextInput
+            name="name"
+            style={styles.input}
+            placeholder="Imię"
+          />
+          <TextInput
+            name="surname"
+            style={styles.input}
+            placeholder="Nazwisko"
+          />
+          <TextInput
+            name="nickname"
+            style={styles.input}
+            placeholder="Nazwa użytkownika"
+          />
+          <TextInput
+            name="mail"
+            style={styles.input}
+            placeholder="Adres e-mail"
+          />
+          <TextInput
+            name="pass1"
+            style={styles.input}
+            placeholder="Hasło"
+          />
+          <TextInput
+            name="pass2"
+            style={styles.input}
+            placeholder="Powtórz hasło"
+          />
         </View>
-      </ScrollView>
-    </Viewport>
+        <View style = {styles.buttonArea}>
+          <TouchableOpacity style={styles.button} onPress={() => console.log("Zapisz")} >
+            <Text style={{fontSize: 18}}>Zarejestruj się</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.info}>Jeżeli posiadasz już konto kliknij
+              <Text style={[styles.info, {color: '#b8215b'}]} onPress={() => { navigation.navigate('AuthView') }}> tutaj. </Text>
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 
