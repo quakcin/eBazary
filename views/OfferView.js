e1e4a5b35a0a8fb27df34db7bea2568c84bad82a
Michael <michas2820@onet.eu>
2022-11-03 17:16:07 +0100

    Price 2 decimal places

--- views/OfferView.js
+++ views/OfferView.js
@@ -140,7 +140,7 @@ export function OfferView({ navigation }) {
                   fontFamily: 'Ubuntu_700Bold'
                 }}
               >
-                {offer.price}zł
+                {offer.price.toFixed(2)}zł
               </Text>
 
               <TouchableOpacity
